b7de243732d81841e5069935f6e51e5f72ea6b9d
huzaifah0x00 <huzaifa.asif@codeninjaconsulting.com>
2022-04-01 13:29:22 +0500

    nvm... peerDependencies don't work as expected.

--- blueprints/ember-cli-react/index.js
+++ blueprints/ember-cli-react/index.js
@@ -1,6 +1,19 @@
 var pkg = require('../../package.json');
 const fs = require('fs');
 
+function getDependencyVersion(packageJson, name) {
+  var dependencies = packageJson.dependencies;
+  var devDependencies = packageJson.devDependencies;
+
+  return dependencies[name] || devDependencies[name];
+}
+
+function getPeerDependencyVersion(packageJson, name) {
+  var peerDependencies = packageJson.peerDependencies;
+
+  return peerDependencies[name];
+}
+
 module.exports = {
   description: 'Install ember-cli-react dependencies into your app.',
 
@@ -8,6 +21,23 @@ module.exports = {
 
   // Install react into host app
   afterInstall: async function () {
+    const packages = [
+      {
+        name: 'react',
+        target: getPeerDependencyVersion(pkg, 'react'),
+      },
+      {
+        name: 'react-dom',
+        target: getPeerDependencyVersion(pkg, 'react-dom'),
+      },
+      {
+        name: '@ember/render-modifiers',
+        target: getDependencyVersion(pkg, '@ember/render-modifiers'),
+      },
+    ];
+
+    await this.addPackagesToProject(packages);
+
     let appFile = fs.readFileSync('app/app.js', 'utf8');
     if (appFile.includes('import Resolver from "ember-resolver"')) {
       appFile = appFile.replace(
